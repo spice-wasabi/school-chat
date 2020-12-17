3c991fe49800f43d5f7553455279d5bbfd962684
spice-wasabi <spice-wasabi@users.noreply.github.com>
2020-12-17 19:50:54 +0900

    学校登録画面作成

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,27 @@
+class SchoolsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @school = School.new
+  end
+
+  def create
+    binding.pry
+    @school = School.new(school_params)
+    if @school.save
+      redirect_to root_path
+      else
+        render :new
+    end
+  end
+
+  private
+
+  def school_params
+    params.require(:school).permit(:school_name)
+  end
+
+
+end
