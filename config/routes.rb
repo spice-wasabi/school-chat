040a17363a95ad688297253a1d2c9442fe24c171
spice-wasabi <izawaism999@gmail.com>
2021-01-12 22:10:57 +0900

    view編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tops#index'
   resources :schools, only: [:new, :create]
+  resources :students, only: [:new, :create]
+  resources :rooms, only: [:index, :new, :create]
 end
